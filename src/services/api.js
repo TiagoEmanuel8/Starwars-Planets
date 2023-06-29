bed44dc214f416b116c76165bbe834e2762b91cf
TiagoEmanuel8 <tiago.emanuel.n@gmail.com>
2023-06-28 21:14:03 -0300

    feat: alterando url api

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 // Req 1 - 1.1 - Uma requisição API usando o async/await
 const getPlanets = async () => {
-  const results = await (await fetch('https://swapi-trybe.herokuapp.com/api/planets/')).json();
+  const results = await (await fetch("https://swapi.dev/api/planets")).json();
   return results;
 };
 
